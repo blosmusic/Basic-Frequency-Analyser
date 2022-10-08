eede52753e3d50771b4ea844b0eeb1f19962bb33
BLOS <blosguitarist@gmail.com>
2022-10-08 17:33:19 +0100

    added animations

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container');
 const canvas = document.getElementById('canvas1');
+const file = document.getElementById('fileupload');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
@@ -29,17 +30,52 @@ container.addEventListener('click', function() {
         x = 0;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         analyser.getByteFrequencyData(dataArray);
-        for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i];
-            const r = barHeight + (25 * (i/bufferLength));
-            const g = 250 * (i/bufferLength);
-            const b = 50;
-            ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
-            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-            x += barWidth + 1;
-        }
+        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
+//to load file
+file.addEventListener('change', function() {
+    const files = this.files;
+    const audio1 = document.getElementById('audio1');
+    audio1.src = URL.createObjectURL(files[0]);
+    audio1.load();
+    audio1.play();
+    const audioCtx = new AudioContext();
+    audioSource = audioCtx.createMediaElementSource(audio1);
+    //process data 
+    analyser = audioCtx.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioCtx.destination); //output data to speakers
+    analyser.fftSize = 512;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    const barWidth = (canvas.width / bufferLength) * 2.5;
+    let barHeight;
+    let x;
+    //draw data
+    function animate() {
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
         requestAnimationFrame(animate);
     }
     animate();
 });
 
+function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray) {
+    for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        const r = barHeight + (25 * (i/bufferLength));
+        const g = 250 * (i/bufferLength);
+        const b = 50;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(x, canvas.height - barHeight - 15, barWidth, 10); //match ln 78 and edit
+        ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth + 1;
+    }
+}
